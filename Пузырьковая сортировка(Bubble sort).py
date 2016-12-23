4135301db086ebfc9eb05506b2048912c3aebc05
marsel1323 <amn132313@gmail.com>
2016-12-23 14:14:10 +0300

    Create Bubble sort on Python 3

---
+++ Пузырьковая сортировка(Bubble sort).py
@@ -0,0 +1,21 @@
+#Сортировка пузырьком в Python3
+#Bubble sort in Python3
+
+import time
+
+begin_time=time.time()
+
+def bubbleSort(List):
+    for i in range(0, len(List) - 1):
+        for j in range(0, len(List) - 1 - i):
+            if List[j] > List[j + 1]:
+                List[j], List[j + 1] = List[j + 1], List[j]
+    return List
+
+A = [1, 3, 2, 10, 5, 8, 6, 7, 9]
+B = ['f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a','f', 'c', 'e', 'd', 'b', 'a']
+
+print(bubbleSort(B))
+
+end_time = time.time()
+print(end_time-begin_time)
