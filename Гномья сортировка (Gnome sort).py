c494f3debf4630a3944013347a79a90b2dbba51f
marsel1323 <amn132313@gmail.com>
2016-12-24 00:04:10 +0300

    Добавлена функция чтения из файла

--- Гномья сортировка (Gnome sort).py
+++ Гномья сортировка (Gnome sort).py
@@ -1,15 +1,18 @@
 #Гномья сортировка в Python3
 #Gnome sort in Python3
 
-import time
-begin_time = time.time()
+import Test, time, os
 
+begin_time = time.time()
+f = open('Test_sort.txt', 'x')
+
+A = Test.rand()
 B = ['f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b',
      'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd',
      'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd',
      'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', '1', '3', '2', '5',
      '8', '6', '7', '9']
-
+#C =
 def gnomeSort(List):
     i=1
     while i < len(List):
@@ -20,6 +23,10 @@ def gnomeSort(List):
             i -= 1
     return List
 
-print(gnomeSort(B))
+f.write('\n')
+f.write('\n')
+f.write(str(gnomeSort(A)))
+print(gnomeSort(A))
+
 end_time = time.time()
 print(end_time-begin_time)
