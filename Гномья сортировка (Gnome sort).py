7ebcdcd2c0ec785118806fabf078255e9255ed2d
marsel1323 <amn132313@gmail.com>
2016-12-23 20:31:51 +0300

    Edit Gnome Sort class

--- Гномья сортировка (Gnome sort).py
+++ Гномья сортировка (Gnome sort).py
@@ -3,22 +3,21 @@
 
 import time
 begin_time = time.time()
+
 B = ['f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b',
      'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd',
-     'b',
-     'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd',
-     'b',
-     'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', '1', '3', '2', '5',
-     '8',
-     '6', '7', '9']
+     'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd',
+     'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', 'f', 'c', 'e', 'd', 'b', 'a', '1', '3', '2', '5',
+     '8', '6', '7', '9']
+
 def gnomeSort(List):
     i=1
-    while i<len(List):
-        if not i or List[i-1]<=List[i]:
-            i+=1
+    while i < len(List):
+        if not i or List[i - 1] <= List[i]:
+            i += 1
         else:
-            List[i], List[i-1]=List[i-1],List[i]
-            i-=1
+            List[i], List[i - 1] = List[i - 1], List[i]
+            i -= 1
     return List
 
 print(gnomeSort(B))
